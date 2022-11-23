fccdbbc5e2c077f643561de8fb7fd696b02c550c
lydiapark02 <noworneversyy@bagsoyeong-ui-MacBookPro.local>
2022-11-24 01:26:53 +0900

    first commit

---
+++ homepages/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $("#sidemenu.html").load("home.html");
+});
+
+// const body = document.querySelector("body"),
+//       sidebar = body.querySelector(".sidebar"),
+//       toggle = body.querySelector(".toggle"),
+
+// // const togglingBtns = document.querySelector('.bx-chevron-right');
+
+// // togglingBtns.addEventListener('click', function(){
+// //     togglingBtns.classList.toggle('bx');
+// // })
+
+//       toggle.addEventlistener("click", () => {
+//         sidebar.classList.toggle("close");
+//       })
+
+//     //   27:12
